73c626359b897d92061192f4e46585706a81e057
Bin Wang <binwang.0213@gmail.com>
2020-11-25 10:12:13 -0600

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+dset="https://zenodo.org/record/4134357/files"
+np=8
+
+lscpu
+
+for p in $( ./problems.py ); do
+    echo
+    echo --- ${p} ---
+    A=${p}_A.bin
+    b=${p}_b.bin
+    u=${p}_u.txt
+    wget ${dset}/${A} ${dset}/${b} ${dset}/${u}
+    partition -B -i ${A} -o part.bin -n ${np}
+    echo
+    echo --- PETSC ---
+    OMP_NUM_THREADS=1 mpirun -np ${np} petsc_fs -A ${A} -f ${b}-u $( cat ${u} ) -p part.bin 
+    echo
+    echo --- AMGCL ---
+    OMP_NUM_THREADS=${np} OMP_PLACES=cores amgcl_spc_block_mixed -A ${A} -f ${b} -u $( cat ${u} )
+    rm ${A} ${b} ${u} part.bin
